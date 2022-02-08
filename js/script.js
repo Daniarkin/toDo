c2995373570941b6099b529e9bb223e7f3995178
daniarkin <jishikukuto@gmail.com>
2022-02-08 19:05:03 +0300

    to do

--- js/script.js
+++ js/script.js
@@ -2,28 +2,54 @@ let draging = false
 const listItems = document.querySelectorAll('li')
 const body = document.body
 const liParent = listItems[0].parentNode
+const dragList = document.getElementById('dragList')
 let draggedItem
+let hoveredItem
 
 
-listItems.forEach(li => li.addEventListener('mousedown', (event) => {
-  event.preventDefault()
-  draging = true
-  console.log(event)
-  draggedItem = event.target
-  dragList.append(draggedItem)
-}))
+listItems.forEach(li => {
+  li.addEventListener('mousedown', (event) => {
+    event.preventDefault()
+    draging = true
+    console.log(event)
+    draggedItem = event.target
+    dragList.append(draggedItem)
+    body.style.cursor = 'grabbing'
+
+  })
+
+  li.addEventListener('mouseover', () => {
+    if (draging) hoveredItem = li
+  })
+
+  li.addEventListener('mouseout', (event) => {
+    if (hoveredItem) hoveredItem.style = ``
+    hoveredItem = null
+  })
+})
 
 body.addEventListener('mouseup', (event) => {
-  draging = true
-  console.log(event)
-  if(draggedItem) liParent.append(draggedItem)
-
+  draging = false
+  if (draggedItem) liParent.insertBefore(draggedItem, hoveredItem)
+  if (hoveredItem) hoveredItem.style = ``
+  hoveredItem = null
+  body.style.cursor = null
   draggedItem = null
 })
 
-body.addEventListener('mousemove', () => {
+body.addEventListener('mousemove', (event) => {
   event.preventDefault()
   if (!draging) return
-  console.log(event)
+  dragList.style.top = `${event.y - 20}px`
+  dragList.style.left = `${event.x - 30}px`
+  if (hoveredItem) hoveredItem.style = `padding-top: 17px ` 
 })
 
+body.addEventListener('mouseleave', () => {
+  draging = false
+  if (draggedItem) liParent.append(draggedItem)
+  if (hoveredItem) hoveredItem.style = ``
+  hoveredItem = null
+  body.style.cursor = null
+  draggedItem = null
+})
